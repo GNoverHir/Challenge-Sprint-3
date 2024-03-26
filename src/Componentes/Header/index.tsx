bc7ddc5f9e8840b2b699dacd5173b8009ffab081
Pedro Lucca Medeiros Miranda <pedroluccaprofissional@gmail.com>
2024-03-26 01:00:21 -0300

    Commit fonts

--- src/Componentes/Header/index.tsx
+++ src/Componentes/Header/index.tsx
@@ -1,10 +1,24 @@
 import { Link } from 'react-router-dom'
 import './style.css'
+import logoSalesforce from '../../assets/logoSalesforce.svg'
+
 
 const Header = () => {
 
     return(
-        <header></header>
+        <header>
+            <div className='Container'>
+                <img width={150} height={100} src={logoSalesforce} alt="Logo Salesforce" className='LogoSalesforce' />
+                <h3><Link to='/sobreNos'>Sobre Nos</Link></h3>
+                <h3><Link to='/produtos'>Produtos</Link></h3>
+                <h3><Link to='/suporte'>Suporte</Link></h3>
+                <div className='EntrarContato'>
+                    <h3>Entre em contato</h3>
+                    <h3>0800 891 1887</h3>
+                </div>
+                <button>Login/Cdastro</button>
+            </div>
+        </header>
     )
 }
 
