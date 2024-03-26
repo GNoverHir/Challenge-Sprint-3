dda742b3c5369835fe2333097b7c184228ee10b7
Pedro Lucca Medeiros Miranda <pedroluccaprofissional@gmail.com>
2024-03-26 01:29:10 -0300

    Commit Header

--- src/Componentes/Header/index.tsx
+++ src/Componentes/Header/index.tsx
@@ -8,15 +8,15 @@ const Header = () => {
     return(
         <header>
             <div className='Container'>
-                <img width={150} height={100} src={logoSalesforce} alt="Logo Salesforce" className='LogoSalesforce' />
-                <h3><Link to='/sobreNos'>Sobre Nos</Link></h3>
-                <h3><Link to='/produtos'>Produtos</Link></h3>
-                <h3><Link to='/suporte'>Suporte</Link></h3>
+                <Link to='/'><img width={150} height={100} src={logoSalesforce} alt="Logo Salesforce" className='LogoSalesforce' /></Link>
+                <h3><Link to='/sobreNos' >Sobre Nos</Link></h3>
+                <h3><Link to='/produtos' >Produtos</Link></h3>
+                <h3><Link to='/suporte' >Suporte</Link></h3>
                 <div className='EntrarContato'>
-                    <h3>Entre em contato</h3>
-                    <h3>0800 891 1887</h3>
+                    <h4>Entre em contato</h4>
+                    <h4>0800 891 1887</h4>
                 </div>
-                <button>Login/Cdastro</button>
+                <button>Login/Cadastro</button>
             </div>
         </header>
     )
