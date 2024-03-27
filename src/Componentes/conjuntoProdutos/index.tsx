21f9354d2794f6e033768b506ab4b8db007e5cfa
Pedro Lucca Medeiros Miranda <pedroluccaprofissional@gmail.com>
2024-03-27 09:22:59 -0300

    Commit Page Produtos

---
+++ src/Componentes/conjuntoProdutos/index.tsx
@@ -0,0 +1,75 @@
+import './style.css'
+import Trailer2 from '../../assets/trailler-2.svg'
+import LogoSalesCloud  from '../../assets/Icon-sales-cloud.svg'
+
+
+const Produtos = () => {
+
+    return(
+        <div>
+            <div className='ConteudoGeralProdutos'>
+                <div className='ConteudosProdutos1'>
+                    <input type="text" id='BarraPesquisa' placeholder='Pesquise o produto'/>
+                    <section>
+                        <img width={33} height={28} src={LogoSalesCloud} alt="" />
+                        <p>Sales Cloud</p>
+                    </section>
+
+                    <section>
+                        <img width={33} height={28} src={LogoSalesCloud} alt="" />
+                        <p>Sales Cloud</p>
+                    </section>
+
+                    <section>
+                        <img width={33} height={28} src={LogoSalesCloud} alt="" />
+                        <p>Sales Cloud</p>
+                    </section>
+
+                    <section>
+                        <img width={33} height={28} src={LogoSalesCloud} alt="" />
+                        <p>Sales Cloud</p>
+                    </section>
+
+                    <section>
+                        <img width={33} height={28} src={LogoSalesCloud} alt="" />
+                        <p>Sales Cloud</p>
+                    </section>
+
+                    <section>
+                        <img width={33} height={28} src={LogoSalesCloud} alt="" />
+                        <p>Sales Cloud</p>
+                    </section>
+
+                    <section>
+                        <img width={33} height={28} src={LogoSalesCloud} alt="" />
+                        <p>Sales Cloud</p>
+                    </section>
+
+                    <section>
+                        <img width={33} height={28} src={LogoSalesCloud} alt="" />
+                        <p>Sales Cloud</p>
+                    </section>
+
+                    <section>
+                        <img width={33} height={28} src={LogoSalesCloud} alt="" />
+                        <p>Sales Cloud</p>
+                    </section>
+                </div>
+
+                <div className='ConteudosProdutos2'>
+                    <section className='SectNomeProduto'>
+                        <img width={116} height={116} src={LogoSalesCloud} alt="" />
+                        <h1>SALES CLOUD!</h1>
+                    </section>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti corrupti, quaerat temporibus voluptatem labore eius, soluta modi facilis illum voluptatibus similique, ex ipsam maxime vero. Totam tempore qui nisi odio. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis aut deserunt at possimus debitis? Blanditiis saepe rem tenetur maiores, quo, sunt id minima aliquid fuga beatae soluta! Odit, tempora distinctio.</p>
+                    <img className='TrailerProduto' width={475} height={175} src={Trailer2} alt="" />
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum iure quo similique cum vero, laboriosam, expedita asperiores pariatur fuga maiores alias? Earum adipisci dolore harum eum modi, expedita blanditiis cumque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita iusto laborum minus tenetur labore quas ex, enim earum ratione aperiam aut distinctio commodi. Magnam aliquam atque obcaecati asperiores earum placeat!</p>
+                    <img className='TrailerProduto' width={475} height={175} src={Trailer2} alt="" />
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur, sapiente animi. Cupiditate reprehenderit harum ipsa, eius ab exercitationem. Molestiae obcaecati dolorum aut in quod recusandae animi quibusdam, eaque quo laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus error obcaecati necessitatibus esse atque unde, vitae rerum repellat, voluptatum sequi minima repudiandae harum, eaque optio? Non, earum culpa? Voluptatibus, vel?</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Produtos
--- src/pages/produtos/index.tsx
+++ src/pages/produtos/index.tsx
@@ -1,14 +1,17 @@
 import { Link } from 'react-router-dom'
 import './style.css'
 import Header from '../../Componentes/header'
+import Footer from '../../Componentes/footer'
+import ConjuntoProdutos from '../../Componentes/conjuntoProdutos'
+
 
 const Produtos = () => {
 
     return(
         <div>
             <Header/>
-            <h1>PRODUTOS</h1>
-            <Link to="/">HOME</Link>
+            <ConjuntoProdutos/>
+            <Footer/>
         </div>
     )
 }
